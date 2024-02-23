6b6ec6f28548e34a7d0e6c0df2462b3a43cc5191
Oliver Hunt <oliverhunt19@gmail.com>
2024-02-23 19:58:53 +0000

    Added the utils project

---
+++ GeneralUtils/NullableTaskHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralUtils
+{
+    public static class NullableTaskHelpers
+    {
+        public static Task NullableTask(this Task? task)
+        {
+            return task.NullableTask(out _);
+        }
+
+        public static Task NullableTask(this Task? task, out bool IsNotNull)
+        {
+            if (task == null)
+            {
+                IsNotNull = false;
+                return Task.CompletedTask;
+            }
+            else
+            {
+                IsNotNull = true;
+                return task;
+            }
+        }
+
+        public static Task<T?> NullableTask<T>(this Task<T?>? task)
+        {
+            return task.NullableTask(out _);
+        }
+
+        public static Task<T?> NullableTask<T>(this Task<T?>? task, out bool IsNotNull)
+        {
+            if (task == null)
+            {
+                IsNotNull = false;
+                return Task.FromResult(default(T));
+            }
+            else
+            {
+                IsNotNull = true;
+                return task;
+            }
+        }
+    }
+}
---
+++ GeneralUtils/TimerAsyncManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralUtils
+{
+    public class TimerAsyncManager
+    {
+        private TimerAsync? timerAsync;
+
+        private Func<CancellationToken, Task> scheduledAction;
+
+        public TimerAsyncManager(Func<CancellationToken, Task> scheduledAction)
+        {
+            this.scheduledAction = scheduledAction;
+        }
+
+        public TimerAsyncManager(Func<Task> scheduledAction) : this((c)=> scheduledAction())
+        {
+
+        }
+
+        public async Task Start(TimeSpan dueTime, TimeSpan period, bool canStartNextActionBeforePreviousIsCompleted = false)
+        {
+            await (timerAsync?.Stop()).NullableTask();
+            timerAsync?.Dispose();
+            timerAsync = new TimerAsync(scheduledAction, dueTime, period, canStartNextActionBeforePreviousIsCompleted);
+            await timerAsync.Start();
+        }
+
+        public Task Start(TimeSpan period, bool canStartNextActionBeforePreviousIsCompleted = false)
+        {
+            return Start(TimeSpan.Zero, period, canStartNextActionBeforePreviousIsCompleted);
+        }
+
+        public Task Start()
+        {
+            if(timerAsync == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return timerAsync.Start();
+        }
+
+        public Task Stop()
+        {
+            return (timerAsync?.Stop()).NullableTask();
+        }
+    }
+}
