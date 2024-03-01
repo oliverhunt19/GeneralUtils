4ee8f1ec4fcba3667e3ae2ed08748f7b3201a9f1
Oliver Hunt <oliver.hunt@renishaw.com>
2024-03-01 12:30:50 +0000

    moved some things around

---
+++ GeneralUtils/TaskHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralUtils
+{
+    public static class TaskHelpers
+    {
+        public static void RunSynchronous(this Task task)
+        {
+            Task.Run(() => task).Wait();
+        }
+
+        public static T RunSynchronous<T>(this Task<T> task)
+        {
+            return Task.Run(() => task).Result;
+        }
+    }
+}
